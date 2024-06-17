8a657968ffe2ce8a902ace920bc1e0cc9c67be10
Mariana <marianabmoraes.ef@gmail.com>
2024-06-17 11:27:15 -0300

    feat: código completo

---
+++ index.js
@@ -0,0 +1,14 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let pares = [];
+let impares = [];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 == 0) {
+    pares.push(numeros[i]);
+  } else {
+    impares.push(numeros[i]);
+  }
+}
+let todos = [...pares, ...impares];
+console.log(todos);
